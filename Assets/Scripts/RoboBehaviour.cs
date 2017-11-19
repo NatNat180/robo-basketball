042393da504c8d87a940a1d25d39a6a74119e67f
Argall, Nathan <Nathan.Argall@gmail.com>
2017-11-19 11:48:43 -0600

    add tank controls to robo behaviour script

--- Assets/Scripts/RoboBehaviour.cs
+++ Assets/Scripts/RoboBehaviour.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class RoboBehaviour : MonoBehaviour {
 
+	private Rigidbody player;
+	private Collider playerCollder;
+	public float moveSpeed;
+	public float rotationSpeed;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		player = GetComponent<Rigidbody>();
+		playerCollder = GetComponent<Collider>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		float rotate = Input.GetAxis("Horizontal");
+		float move = Input.GetAxis("Vertical");
+
+		player.velocity = transform.forward * moveSpeed * move;
+		transform.Rotate(Vector3.up * rotate * rotationSpeed * Time.deltaTime);
 		
 	}
 }

f252a05ccecbbad7ae70ce21fd77a20755ef6c8a
Argall, Nathan <Nathan.Argall@gmail.com>
2017-11-19 18:51:52 -0600

    add scoop tag and script, ball flies only on trigger enter with scoop

--- Assets/Scripts/ShootBall.cs
+++ Assets/Scripts/ShootBall.cs
@@ -8,34 +8,24 @@ public class ShootBall : MonoBehaviour
     Animator animator;
     public Rigidbody basketball;
     public float thrust;
-    bool isHoldingBall;
 
     // Use this for initialization
     void Start()
     {
-
         animator = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         /* set up shoot animation trigger on button down */
         bool shoot = Input.GetButtonDown("Shoot");
         animator.SetBool("shoot", shoot);
-        if (shoot && isHoldingBall)
+        if (shoot && ScoopDetector.isInScoop)
         {
             basketball.AddForce(Vector3.up * thrust, ForceMode.Impulse);
+			ScoopDetector.isInScoop = false;
         }
     }
-    private void OnCollisionEnter(Collision col)
-    {
-		Debug.Log("collision detected!");
-        if (col.gameObject.tag == "Basketball")
-        {
-            isHoldingBall = true;
-        }
-    }
+
 }

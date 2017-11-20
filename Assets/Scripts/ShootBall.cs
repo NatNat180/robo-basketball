d9387704d9a8384427f06ada4286f2e221a9d954
Argall, Nathan <Nathan.Argall@gmail.com>
2017-11-19 18:09:23 -0600

    add collision detection

--- Assets/Scripts/ShootBall.cs
+++ Assets/Scripts/ShootBall.cs
@@ -2,28 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShootBall : MonoBehaviour {
+public class ShootBall : MonoBehaviour
+{
 
-	Animator animator;
-	public Rigidbody basketball;
-	public float thrust;
+    Animator animator;
+    public Rigidbody basketball;
+    public float thrust;
+    bool isHoldingBall;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-		animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-		/* set up shoot animation trigger on button down */
-		bool shoot = Input.GetButtonDown("Shoot");
-		animator.SetBool("shoot", shoot);
-		if (shoot) 
-		{
-			basketball.AddForce(Vector3.up * thrust, ForceMode.Impulse);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        /* set up shoot animation trigger on button down */
+        bool shoot = Input.GetButtonDown("Shoot");
+        animator.SetBool("shoot", shoot);
+        if (shoot && isHoldingBall)
+        {
+            basketball.AddForce(Vector3.up * thrust, ForceMode.Impulse);
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+		Debug.Log("collision detected!");
+        if (col.gameObject.tag == "Basketball")
+        {
+            isHoldingBall = true;
+        }
+    }
 }

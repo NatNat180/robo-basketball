0468a86fda1cfca3c2a3af2674d33c1235d8727e
AndrewJeannett <andrewjeannett@gmail.com>
2017-11-20 09:23:38 -0600

    2nd player

---
+++ Assets/Scripts/RoboBehaviour2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboBehaviour2 : MonoBehaviour {
+
+	private Rigidbody player;
+	private Collider playerCollder;
+	public float moveSpeed;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		player = GetComponent<Rigidbody>();
+		playerCollder = GetComponent<Collider>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		float rotate = Input.GetAxis("Horizontal2");
+		float move = Input.GetAxis("Vertical2");
+
+		player.velocity = transform.forward  * moveSpeed * move;
+		transform.Rotate(Vector3.up * rotationSpeed * rotate * Time.deltaTime);
+		
+	}
+}
---
+++ Assets/Scripts/ShootBall2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall2 : MonoBehaviour
+{
+
+    Animator animator;
+    public Rigidbody basketball;
+    public float thrust;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* set up shoot animation trigger on button down */
+        bool shoot = Input.GetButtonDown("Shoot2");
+        animator.SetBool("shoot", shoot);
+
+		/* apply conditions before shooting ball */
+        if (shoot && ScoopDetector.isInScoop)
+        {
+			Vector3 targetTrajectory = -(transform.forward / 1.5f) + (transform.up * 3);
+            basketball.AddForce(targetTrajectory * thrust, ForceMode.Impulse);
+			ScoopDetector.isInScoop = false;
+        }
+
+    }
+
+}

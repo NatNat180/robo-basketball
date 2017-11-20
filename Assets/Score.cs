e54dc5133a27a4e7c5109fc402a426211aebc00c
AndrewJeannett <andrewjeannett@gmail.com>
2017-11-19 22:54:33 -0600

    scoreboard

---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static int RedScore=1;
+    public static int BlueScore=2;
+    
+
+    // Use this for initialization
+    void Start () {
+      
+        GetComponent<TextMesh>().text = ""+RedScore+" "+BlueScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

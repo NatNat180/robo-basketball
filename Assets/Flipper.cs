bf25a5a4c71473d603ce95e7ceebcf2ef4396e18
AndrewJeannett <andrewjeannett@gmail.com>
2017-12-02 12:54:07 -0600

    Hinge Joint

---
+++ Assets/Flipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+    public bool isActive;
+    HingeJoint myJoint;
+
+	// Use this for initialization
+	void Start () {
+        myJoint = GetComponent<HingeJoint>();
+        GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetButton("Shoot")&& isActive == false)
+        {
+            isActive = true;
+            JointSpring spring = myJoint.spring;
+            spring.targetPosition = -50;
+            myJoint.spring = spring;
+
+            Debug.Log("sprung");
+        }
+        else if (Input.GetButton("Shoot")==false && isActive == true){
+
+            isActive = false;
+            JointSpring spring = myJoint.spring;
+            spring.targetPosition = 1;
+            myJoint.spring = spring;
+        }
+    }
+}
